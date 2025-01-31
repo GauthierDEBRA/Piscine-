9bac3e54614f7f2f941afe060154e52925794a20
Gauthier <gauthier.debra@outlook.fr>
2025-01-31 13:09:08 +0100

    question numéro 2

--- main-3.py
+++ main-3.py
@@ -1,105 +1,79 @@
-liste = [("Pierre","Dos",10),("Paul","Brasse",13),("Léa","Crawl",6), ("Léa","Brasse",8) ]
-commande = ''
+liste = [("Pierre", "Dos", 10), ("Paul", "Brasse", 13), ("Léa", "Crawl", 6), ("Léa", "Brasse", 8)]
+
+def afficher_menu():
+    """Affiche le menu des options disponibles."""
+    print("\n---- Menu ----")
+    print("1 -> Ajouter une performance")
+    print("2 -> Ajouter un individu")
+    print("3 -> Ajouter une nouvelle nage")
+    print("4 -> Lister toutes les performances")
+    print("5 -> Lister les performances d'un nageur")
+    print("6 -> Lister tous les nageurs pratiquant une nage")
+    print("7 -> Sauvegarder les données")
+    print("8 -> Charger les données")
+    print("0 -> Quitter le logiciel")
 
 def cmd_ajout(liste):
     """Ajoute un évènement à la liste"""
-    a = input("Qui nage ? ")
-    b = input("quelle nage ? ")
-    c = input("combien de longueur ? ")
-    liste.append((a,b,c))
+    a = input("Nom du nageur : ")
+    b = input("Type de nage : ")
+    c = input("Nombre de longueurs : ")
+    liste.append((a, b, c))
 
 def cmd_liste(liste):
     """Affiche toutes les performances des nageurs"""
-    print("Prénom      |  nage   |  longueur")
+    print("\nPrénom      |  Nage    |  Longueur")
     print("---------------------------------")
     for elt in liste:
         print(f" {elt[0]:11}| {elt[1]:8}|  {elt[2]}")
 
 def cmd_nageur(liste):
     """Affiche toutes les performances d'un nageur"""
-    tmp = input("Quel nageur ? ")
-    print("Performances de ", tmp)
-    print("  nage   |  longueur")
+    tmp = input("Nom du nageur : ")
+    print(f"\nPerformances de {tmp}")
+    print("  Nage   |  Longueur")
     print("--------------------")
     for elt in liste:
-        if elt[0]== tmp:
+        if elt[0] == tmp:
             print(f" {elt[1]:8}|  {elt[2]}")
 
 def cmd_nage(liste):
     """Affiche toutes les performances suivant une nage donnée"""
-    tmp = input("Quel nage ? ")
-    print("Nage ", tmp)
-    print(" Nageur     |  longueur")
+    tmp = input("Type de nage : ")
+    print(f"\nNage : {tmp}")
+    print(" Nageur     |  Longueur")
     print("------------------------")
     for elt in liste:
-        if elt[1]== tmp:
+        if elt[1] == tmp:
             print(f" {elt[0]:11}|  {elt[2]}")
 
-def cmd_exit(liste):
-    tmp = input("En êtes-vous sûr ? (o)ui/(n)on ")
-    if tmp == 'o':
-        cmd_save(liste, 'save.backup')
-        return False
-    else:
-        return True
-
-def cmd_save(liste, filename):
-    '''sauvegarde la BDD'''
-    fichier = open(filename, 'w')
-    for elt in liste:
-        fichier.write(elt[0]+','+elt[1]+','+str(elt[2])+"\n")
-    fichier.close()
-
-def cmd_load(liste, filename):
-    'charge la BDD'
-    fichier = open(filename, 'r')
-    for line in fichier:
-        line.strip()
-        if line[-1] == '\n':
-            line = line[:-1]
-        if line[0]=='#':
-            continue
-        tmp = line.split(',')
-        liste.append(tuple(tmp))
-    fichier.close()
-
-
-def get_cmd():
-    '''Traitement de la commande d'entrée'''
-    msg = input("Que faut-il faire ? ")
-    msg = msg.lower()
-    return msg
+def cmd_exit():
+    """Quitte le programme"""
+    print("Au revoir !")
+    return False
 
 isAlive = True
 while isAlive:
-    commande = get_cmd()
+    afficher_menu()  # Affiche le menu avant chaque saisie
+    commande = input("Votre choix : ")  # Saisie de la commande
 
-    if commande == 'ajout':
+    if commande == "1":
         cmd_ajout(liste)
-        continue
-   
-    if commande == 'liste':
+    elif commande == "2":
+        print("Ajout d'un individu (Non implémenté)")
+    elif commande == "3":
+        print("Ajout d'une nouvelle nage (Non implémenté)")
+    elif commande == "4":
         cmd_liste(liste)
-        continue
-
-    if commande == 'nageur':
+    elif commande == "5":
         cmd_nageur(liste)
-        continue
-
-    if commande == 'nage':
+    elif commande == "6":
         cmd_nage(liste)
-        continue
-
-    if commande == 'save':
-        cmd_save(liste, 'save.csv')
-        continue
-
-    if commande == 'load':
-        cmd_load(liste, 'save.csv')
-        continue
-
-    if commande == 'exit':
-        isAlive = cmd_exit(liste)
-        continue
-
-    print(f"Commande {commande} inconnue")
+    elif commande == "7":
+        print("Sauvegarde des données (Non implémenté)")
+    elif commande == "8":
+        print("Chargement des données (Non implémenté)")
+    elif commande == "0":
+        isAlive = cmd_exit()  # Quitter l'application
+    else:
+        print("Commande invalide. Veuillez choisir un numéro du menu.")
